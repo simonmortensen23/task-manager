aa938e04b46cc434bd58f5c0fefc626a17bc8fad
simonmortensen23 <simon020393@gmail.com>
2022-06-23 11:04:41 +0000

    failuire

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from taskmanager import app
 
 if __name__ == "__main__":
     app.run(
-        host= os.environ.get("IP"),
+        host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG"),
     )
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -8,11 +8,12 @@ class Category(db.Model):
     tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
+        # __repr__ to represent itself in the form of a string
         return self.category_name
 
 
 class Task(db.Model):
-    # schema for the Category model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
@@ -21,6 +22,7 @@ class Task(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return "{0} - Task: {1} | Urgent: {2}".format(
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
             self.id, self.task_name, self.is_urgent
         )
